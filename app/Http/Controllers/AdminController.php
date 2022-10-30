c05c98f2c4704b8833c693a51f4e884417b65630
Omidiora Emmanuel <45630269+omidiora@users.noreply.github.com>
2022-10-30 02:09:56 +0100

    adedd

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -192,7 +192,7 @@ class AdminController extends Controller
 
     public function adminViewCategory($id)
     {
-        $categories  =  DB::table('categories')->where('products.id', $id)
+        $categories  =  DB::table('categories')->where('products.category', $id)
             ->join('products', 'products.category', '=', 'categories.id')->get();
 
 
